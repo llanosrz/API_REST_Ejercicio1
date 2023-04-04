ec701f8344b98cf79b037edbd5c313eb960be222
llruiz <llanosrzm@gmail.com>
2023-04-04 10:26:43 +0200

    Borrar cliente

--- src/main/java/com/example/controllers/ClienteController.java
+++ src/main/java/com/example/controllers/ClienteController.java
@@ -6,12 +6,14 @@ import java.util.List;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataAccessException;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -19,7 +21,8 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import com.example.entities.Cliente;
 import com.example.services.ClienteService;
-import com.example.utilities.FileUploadUtil;
+
+import jakarta.transaction.Transactional;
 
 @RestController
 @RequestMapping("/clientes")
@@ -28,45 +31,6 @@ public class ClienteController {
     @Autowired
     private ClienteService clienteService;
 
-    @Autowired
-    private FileUploadUtil fileUploadUtil;
-
-    @GetMapping
-    public ResponseEntity<List<Cliente>> findAll(@RequestParam(name = "page", required = false) Integer page,
-            @RequestParam(name = "size", required = false) Integer size) {
-
-        ResponseEntity<List<Cliente>> responseEntity = null;
-        List<Cliente> clientes = new ArrayList<>();
-        Sort sortByNombre = Sort.by("nombre");
-
-        if (page != null && size != null) {
-
-            try {
-
-                Pageable pageable = PageRequest.of(page, size, sortByNombre);
-
-                Page<Cliente> clientesPaginados = clienteService.findAll(pageable);
-
-                clientes = clientesPaginados.getContent();
-
-                responseEntity = new ResponseEntity<List<Cliente>>(clientes, HttpStatus.OK);
-
-            } catch (Exception e) {
-                responseEntity = new ResponseEntity<>(HttpStatus.BAD_REQUEST);
-            }
-
-        } else {
-            try {
-                clientes = clienteService.findAll(sortByNombre);
-                responseEntity = new ResponseEntity<List<Cliente>>(clientes, HttpStatus.OK);
-            } catch (Exception e) {
-                responseEntity = new ResponseEntity<>(HttpStatus.NO_CONTENT);
-            }
-
-        }
-
-        return responseEntity;
-    }
 
     @GetMapping("/{id}")
     public ResponseEntity<Map<String, Object>> findById(@PathVariable(name = "id") Integer id) {
@@ -106,6 +70,80 @@ public class ClienteController {
 
         return responseEntity;
     }
+
+
+    @DeleteMapping("/{id}")
+    @Transactional
+    public ResponseEntity<String> delete(@PathVariable(name = "id") Integer id) {
+        
+        ResponseEntity<String> responseEntity = null;
+
+        try {
+            //Recuperarlo
+
+            Cliente cliente = clienteService.findById(id);
+
+            if(cliente != null) {
+                clienteService.delete(cliente);
+                responseEntity = new ResponseEntity<String>("Borrado exitosamente", HttpStatus.OK);
+
+            }else{
+                responseEntity = new ResponseEntity<String>("No existe el cliente", HttpStatus.NO_CONTENT);
+
+            }
+
+        } catch (DataAccessException e ) {
+            e.getMostSpecificCause();
+            responseEntity = new ResponseEntity<String>("Error fatal", HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+
+        return responseEntity;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Cliente>> findAll
+    (@RequestParam(name = "page", required = false) Integer page,
+            @RequestParam(name = "size", required = false) Integer size) {
+
+        ResponseEntity<List<Cliente>> responseEntity = null;
+
+        List<Cliente> clientes = new ArrayList<>();
+
+        Sort sortByNombre = Sort.by("nombre");
+
+        if (page != null && size != null) {
+
+            try {
+
+                Pageable pageable = PageRequest.of(page, size, sortByNombre);
+
+                Page<Cliente> clientesPaginados = clienteService.findAll(pageable);
+
+                clientes = clientesPaginados.getContent();
+
+                responseEntity = new ResponseEntity<List<Cliente>>(clientes, HttpStatus.OK);
+
+            } catch (Exception e) {
+                responseEntity = new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+            }
+
+        } else {
+
+            // Sin paginación pero con ordenamiento
+
+            try {
+                clientes = clienteService.findAll(sortByNombre);
+                responseEntity = new ResponseEntity<List<Cliente>>(clientes, HttpStatus.OK);
+            } catch (Exception e) {
+                responseEntity = new ResponseEntity<>(HttpStatus.NO_CONTENT);
+            }
+
+        }
+
+        return responseEntity;
+    }
+
+   
 
     
 }
